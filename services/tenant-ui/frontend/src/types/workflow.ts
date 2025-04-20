b6c942edfc8aba8a1a0a63ff3519c9d6ff4cbe32
Michael <mike.95.f@gmail.com>
2025-04-20 06:30:05 +0530

    Added Workflow Editor with render view

---
+++ services/tenant-ui/frontend/src/types/workflow.ts
@@ -0,0 +1,51 @@
+export interface Workflow {
+  workflow_id: string;
+  name: string;
+  initial_state: string;
+  render: any;
+  states: State[];
+}
+
+export interface State {
+  name: string;
+  state_id: string;
+  actions: Action[];
+  transitions: Transition[];
+  display_data: DisplayData[];
+}
+
+export interface Action {
+  type: string;
+  value: any;
+  action_id: string;
+  condition: string;
+}
+
+export interface Transition {
+  type: string;
+  state_id: string;
+  condition: string;
+  workflow_id: string;
+  transition_id: string;
+}
+
+export type DisplayData =
+  | {
+      type: "image";
+      url: string;
+    }
+  | {
+      type: "text";
+      text: string;
+      display?: "title";
+      condition?: string;
+    }
+  | {
+      type: "extended";
+      text: string;
+    }
+  | {
+      type: "button";
+      label: string;
+      actionID: string;
+    };
