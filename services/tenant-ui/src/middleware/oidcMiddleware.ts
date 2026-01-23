35c45354df08993055ec75329698d5bb0fb7d13a
Dave McKay <dave@promulgareconsulting.com>
2026-01-23 15:00:13 -0500

    Revert "Feature: OpenID Connect (With Google) Support"

--- services/tenant-ui/src/middleware/oidcMiddleware.ts
+++ services/tenant-ui/src/middleware/oidcMiddleware.ts
@@ -1,7 +1,6 @@
 import { createRemoteJWKSet, jwtVerify, JWTPayload } from "jose";
 import config from "config";
 import { Request, Response, NextFunction } from "express";
-import axios from "axios";
 
 // Extend Express Request to include claims
 interface AuthenticatedRequest extends Request {
@@ -31,42 +30,4 @@ const oidcMiddleware = async (
   }
 };
 
-const oidcGoogleMiddleware = async (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const authHeader = req.headers.authorization;
-    const { state, response } = req.body;
-    
-    // Fetch .well-known/openid-configuration to get token_endpoint
-    const authority: string = config.get("server.oidc.authority");
-    const configId: string = config.get("server.oidc.clientId");
-    const tenantId: string = config.get("server.innkeeper.user");
-    const configSecret: string = config.get("server.oidc.clientSecret");
-    const oid_config = await axios.get(`${authority}/.well-known/openid-configuration`);
-    const tokenEndpoint = oid_config.data.token_endpoint;
-    
-    // Exchange code
-    const tokenResponse = await axios.post(tokenEndpoint, {
-      grant_type: "authorization_code",
-      code: response.code,
-      code_verifier: state.code_verifier,
-      client_id: configId,
-      client_secret: configSecret,
-      redirect_uri: state.redirect_uri,
-    });
-
-    const token = tokenResponse.data.id_token;
-    const { payload } = await jwtVerify(token, jwks);
-    req.claims = payload;
-    next();
-  } catch (error) {
-    console.error("OIDC Middleware Error:", error);
-    res.status(401).json({ message: "Invalid token", error });
-  }
-};
-
 export default oidcMiddleware;
-export { oidcGoogleMiddleware };
